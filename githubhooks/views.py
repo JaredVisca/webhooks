fcda2953b2b9621b0e3bdc59d2fa3f34a765ee70
Jared Visca <jared.visca@joann.com>
2016-04-05 13:54:11 -0400

    Wooohoooo testing more pushing babyyyyyy.

--- githubhooks/views.py
+++ githubhooks/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
 
+@csrf_exempt
 def index(request):
     if request.method == 'POST':
         return HttpResponse(json.dumps(request), content_type="application/json")
