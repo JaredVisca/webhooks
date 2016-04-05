f95d052759229cd6d39bc57917e5fd56a3b8e4e0
Jared Visca <jared.visca@joann.com>
2016-04-05 11:27:25 -0400

    testing push.

--- githubhooks/views.py
+++ githubhooks/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 import json
 
 def index(request):
-    return HttpResponse(json.dumps(request), content_type="application/json")
+    if request.method == 'POST':
+        return HttpResponse(json.dumps(request), content_type="application/json")
+    return HttpResponse("This message is because the request.method is not a POST.")
