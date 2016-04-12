1de93760a0ec88e630bb7e5b66e62060a99f1216
Jared Visca <jared.visca@joann.com>
2016-04-12 14:24:47 -0400

    let's see what happens

--- githubhooks/views.py
+++ githubhooks/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-#comment!
+#what's going to happen this time
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
